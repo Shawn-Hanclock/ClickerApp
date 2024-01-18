c190f84e1dac5dde3e12664495d6cfba320657a1
Shawn-Hanclock <nwahs007@gmail.com>
2024-01-18 13:05:25 -0800

    feat: Added the click counter in Main, each click increases total.

--- app/src/main/java/com/example/clickerapp/MainActivity.java
+++ app/src/main/java/com/example/clickerapp/MainActivity.java
@@ -1,17 +1,28 @@
 package com.example.clickerapp;
 
 import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
-import android.widget.ImageView;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
 
 public class MainActivity extends AppCompatActivity {
-
+    public Button clickBtn;
+    public TextView clicksTv;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        ImageView iv= findViewById(R.id.imageView);
-        iv.setImageResource(R.drawable.ai_duck);
+
+        //add resources being loaded
+        clicksTv = (TextView) findViewById(R.id.clickTotal);
+        clickBtn = (Button) findViewById(R.id.clickButton);
+    }
+
+    public int clicks = 0; //click counter variable
+    public void btnClick(View view) {
+        clicks++;
+        clicksTv.setText(clicks+"");
     }
 }
